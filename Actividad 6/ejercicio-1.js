9a8c9dc4ea2bf26e0d40b7ae91ca72b24b07cc36
augus <augustojpintob@gmail.com>
2022-02-01 22:53:42 -0300

    Métodos de arrays

---
+++ Actividad 6/ejercicio-1.js
@@ -0,0 +1,4 @@
+var animals = ['eagle','parrot', 'monkey', 'boar', 'lion'];
+var comingSoonAnimals = ['panter', 'dragon', 'turtle']
+
+var allAnimals = animals.concat(comingSoonAnimals)
---
+++ Actividad 6/ejercicio-4.js
@@ -0,0 +1,3 @@
+var animals = ['eagle','parrot', 'monkey', 'boar', 'lion'];
+animals.splice(0,1)
+console.log(animals)
