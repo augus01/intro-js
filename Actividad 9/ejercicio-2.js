a1a0ead533bc7f8506576d7e8611b73584bee5bd
augus <augustojpintob@gmail.com>
2022-02-09 00:00:00 -0300

    Se arreglan problemas del ejercicio 2

--- Actividad 9/ejercicio-2.js
+++ Actividad 9/ejercicio-2.js
@@ -9,21 +9,9 @@
     };
  */
 var student = {
-    name: {
-        enumerable: true,
-        value: "David Rayy"
-    },
-    sclass: {
-        enumerable: true,
-        value: "VI"
-    },
-    rollno: {
-        enumerable: true,
-        value: 12
-    }
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
 };
-var propiedadesStudent = (Object.keys(student));
+console.log(Object.keys(student));
 
-for (var i = 0; i < propiedadesStudent.length; i++) {
-    console.log(`La propiendad ${i} es ${propiedadesStudent[i]}`)
-}
